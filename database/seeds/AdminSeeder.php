62555d5cc9932628e6f86d889891774cb486eec1
ArenGr <arengr.1990@gmail.com>
2020-12-08 13:44:34 +0400

    seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->updateOrInsert([
+            'name' => 'admin1',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}

6aabe44216173712addd300f59ecb8cf86eabda3
ArenGr <arengr.1990@gmail.com>
2020-12-08 13:50:59 +0400

    welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Route::get('/', function () { */
+/*     return view('welcome'); */
+/* }); */
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/guest', 'Admin\ProductController@show')->name('show');
+Route::get('/', 'HomeController@index')->name('show');
 
 Route::prefix('/admin')->name('admin.')->namespace('Admin')->group(function(){
 
